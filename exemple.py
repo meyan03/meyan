1d161d26e4a4978125aa2397dd4d38f504757a37
meyan03 <153922594+meyan03@users.noreply.github.com>
2023-12-24 17:27:41 +0100

    Add files via upload

---
+++ exemple.py
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mon Site Web</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+
+        header {
+            background-color: #333;
+            color: white;
+            text-align: center;
+            padding: 1em;
+        }
+
+        main {
+            padding: 2em;
+        }
+
+        footer {
+            background-color: #333;
+            color: white;
+            text-align: center;
+            padding: 1em;
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+
+    <header>
+        <h1>Mon Site Web</h1>
+    </header>
+
+<main>
+    <h2>Bienvenue sur mon site !</h2>
+    <p>Tu as envie d'entreprendre ?</p>
+    <p>Mais t'as des peurs ?</p>
+    <p>Tu te poses plusieurs questions ?</p>
+    <p>Alors permet-moi de te dire plusieurs choses :</p>
+    <img src="echec.jpeg" alt="L'échec est souvent une étape vers le succès">
+    <h3>Quelques plans d'action pour toi !</h3>
+    <img src="preparer.jpg" alt="Acquérir des connaissances sur le domaine d'activité et les compétences nécessaires">
+    <img src="plan.jpg" alt="Établir un plan d'affaires détaillé">
+    <img src="mentor.png" alt="Rejoindre des réseaux d'entrepreneurs">
+    <img src="economie.jpg" alt="Gérer les finances avec prévoyance">
+    <img src="confiance.jpeg" alt="Accepter les défis et rester confiant face aux obstacles.">
+</main>
+
+    <footer>
+        <p>&copy; TRAORE Meyan </p>
+    </footer>
+
+</body>
+</html>
